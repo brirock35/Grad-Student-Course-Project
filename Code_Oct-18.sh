a9d60dcf1629fd08c95b2ff42d87ea1aaf20eff2
brirock35 <54723867+brirock35@users.noreply.github.com>
2019-11-13 19:33:12 -0600

    Update Code_Oct-18.sh

--- Code_Oct-18.sh
+++ Code_Oct-18.sh
@@ -18,8 +18,8 @@ plot(s)
 plot(d1,add= TRUE)
 dev.off()
 #reset res to 1 
-# d1 <- fishnet(s, res=2)
-# d1$grids <- paste0("v", 1:nrow(d1))
+d1 <- fishnet(s, res=2)
+d1$grids <- paste0("v", 1:nrow(d1))
 writeOGR(d1, dsn = "/Users/darulab/Desktop/Brianna R (SPD)/Data/ShapeFiles/grids", layer = "grids_200km", driver = "ESRI Shapefile", overwrite_layer = TRUE)
 
 
@@ -28,3 +28,27 @@ names(df) <- c("lon", "lat", "basis")
 df <- df[complete.cases(df),]
 coordinates(df) <- ~lon+lat
 proj4string(df) <- CRS("+proj=longlat +datum=WGS84")
+
+## 2.generating species richness
+ss <- readOGR(dsn = "/Users/darulab/Desktop/Brianna R (SPD)/Data/ShapeFiles/Seagrasses_SHP_raw", layer = "SEAGRASSES")
+proj4string(ss) <- CRS("+proj=longlat +datum=WGS84")
+
+S <- as.character(unique(ss$binomial))
+
+# for loop to count number of seagrasses per grid cell by species (72 species total)
+out <- NULL
+for (i in 1:length(S)) {
+  x1 <- subset(ss, ss$binomial %in% S[i])
+  proj4string(x1) <- proj4string(s1)
+  x2 <- over(s1, x1)
+  x3 <- cbind(data.frame(s1), x2)
+  M <- as.data.frame.matrix(table(x3$grids, x3$binomial))
+  M[M>0] <- 1
+  M1 <- picante::matrix2sample(M)
+  w <- M1[,c(1,3)]
+  names(w) <- c("grids", "species")
+  out <- rbind(out, w)
+  print(i)
+}
+
+r <- data.frame(out)
